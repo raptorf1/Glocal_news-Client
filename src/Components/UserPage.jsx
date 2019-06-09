73c5bf3aecb35d9eab22da5078bbb32fea9e1bf1
raptorf1 <raptor_f1@hotmail.com>
2019-06-09 19:13:22 +0200

    Gives UserPage component access to redux store. Adjusts acceptance test. Adds text to UserPage component so that test goes green. Axios call remains to fetch user articles.

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { connect } from 'react-redux'
 
 class UserPage extends Component {
   state = {
-    
   }
-  render () {
+  render() {
+    let str = this.props.currentUser.attributes.uid
+    let nameMatch = str.match(/^([^@]*)@/);
+    let name = nameMatch ? nameMatch[1] : null;
+    let upperName = name.charAt(0).toUpperCase() + name.slice(1)
+
     return (
-      <p>yay!</p>
+      <>
+        <p>Hello {upperName}. This is your private page.</p>
+        <p>Your published articles</p>
+        <p>Your unpublished articles</p>
+        <p>Reviews you have left</p>
+      </>
     )
   }
 }
 
-export default UserPage
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.reduxTokenAuth.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(UserPage)
