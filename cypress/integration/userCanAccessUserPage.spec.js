102ed2077f5024620dc901bea57fd300746274c3
raptorf1 <raptor_f1@hotmail.com>
2019-06-09 15:42:29 +0200

    Creates acceptance test for user page

---
+++ cypress/integration/userCanAccessUserPage.spec.js
@@ -0,0 +1,41 @@
+describe('User can view user page', () => {
+
+  beforeEach(function () {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3002/api/v1/articles',
+      response: 'fixture:list_of_articles.json',
+      status: 200
+    })
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3002/api/v1/categories',
+      response: 'fixture:categories_list.json',
+      status: 200
+    })
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3002/api/v1/auth/sign_in',
+      response: 'fixture:successful_login.json',
+      headers: {
+        "uid": "boa@mail.com"
+      }
+    })
+    cy.visit('http://localhost:3001')
+  })
+
+  it('after he logs in and clicks on Welcome link', () => {
+
+    cy.get("#login").click()
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('boa@mail.com')
+      cy.get('#password').type('password')
+    })
+    cy.get('#user_page').click()
+    cy.contains('Hello Boa. This is your private page.')
+    cy.contains('Your published articles')
+    cy.contains('Your unpublished articles')
+    cy.contains('Reviews you have left')
+  })
+})
