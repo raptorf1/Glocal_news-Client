6c961c2ab221e42dc67504f591c998fbae8a4744
zanenkn <zane.neikena@gmail.com>
2019-06-09 21:34:18 +0200

    Refactors the UserPage and adds other loops to display users unpublished and declined articles.

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
-import { Container, Grid } from 'semantic-ui-react'
+import { Container, Grid, Segment, Icon, Header } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 class UserPage extends Component {
   state = {
@@ -15,48 +16,67 @@ class UserPage extends Component {
   }
 
   render() {
-    let str = this.props.currentUser.attributes.uid
-    let nameMatch = str.match(/^([^@]*)@/);
-    let name = nameMatch ? nameMatch[1] : null;
-    let upperName = name.charAt(0).toUpperCase() + name.slice(1)
+    let publishedArticlesToDisplay
+    let unpublishedArticlesToDisplay
+    let declinedArticlesToDisplay
+    let upperName
 
-    let publishedArticles = []
-    let unpublishedArticles = []
-    let declinedArticles = []
-
-    this.state.articles.map(article => {
-      if (article.user.id == this.props.currentUser.attributes.id) {
-        if (article.published === true) {
-          return publishedArticles.push(article)
-        } else if (article.published === false && article.reviews.length < 3) {
-          return unpublishedArticles.push(article)
-        } else if (article.published === false && article.reviews.length >= 3) {
-          return declinedArticles.push(article)
+    if (this.props.currentUser.isSignedIn === true) {
+      let str = this.props.currentUser.attributes.uid
+      let nameMatch = str.match(/^([^@]*)@/);
+      let name = nameMatch ? nameMatch[1] : null;
+      upperName = name.charAt(0).toUpperCase() + name.slice(1)
+  
+      let publishedArticles = []
+      let unpublishedArticles = []
+      let declinedArticles = []
+  
+      this.state.articles.map(article => {
+        if (article.user.id == this.props.currentUser.attributes.id) {
+          if (article.published === true) {
+            return publishedArticles.push(article)
+          } else if (article.published === false && article.reviews.length < 3) {
+            return unpublishedArticles.push(article)
+          } else if (article.published === false && article.reviews.length >= 3) {
+            return declinedArticles.push(article)
+          }
         }
-      }
-    })
-
-    let publishedArticlesToDisplay = (
-      publishedArticles.map(article => {
-        return (
-          <Segment id={article.id} color={color} key={article.id}>
-            <Grid columns={2}>
-              <Grid.Column floated='left' width={11}>
-              </Grid.Column>
-              <Grid.Column floated='right' width={5}>
-                <Label tag style={{ backgroundColor: `${color}`, color: 'white' }}>
-                  {article.category.name}
-                </Label>
-              </Grid.Column>
-            </Grid>
-
-            <Header id={`title_${article.id}`} as={Link} to={{ pathname: '/full-article', state: { id: `${article.id}` } }}>{article.title}</Header>
-            <p id={`country_city_${article.id}`}><Icon name='map marker alternate' />{`${article.city}, ${article.country}`}</p>
-          </Segment>
-        )
       })
+  
+      publishedArticlesToDisplay = (
+        publishedArticles.map(article => {
+          return (
+            <Segment id={article.id} key={article.id}>
+              <Header id={`title_${article.id}`} as={Link} to={{ pathname: '/full-article', state: { id: `${article.id}` } }}>{article.title}</Header>
+              <p id={`country_city_${article.id}`}><Icon name='map marker alternate' />{`${article.city}, ${article.country}`}</p>
+            </Segment>
+          )
+        })
+      )
+  
+      unpublishedArticlesToDisplay = (
+        unpublishedArticles.map(article => {
+          return (
+            <Segment id={article.id} key={article.id}>
+              <Header id={`title_${article.id}`} as={Link} to={{ pathname: '/full-article', state: { id: `${article.id}` } }}>{article.title}</Header>
+              <p id={`country_city_${article.id}`}><Icon name='map marker alternate' />{`${article.city}, ${article.country}`}</p>
+            </Segment>
+          )
+        })
+      )
+  
+      declinedArticlesToDisplay = (
+        declinedArticles.map(article => {
+          return (
+            <Segment id={article.id} key={article.id}>
+              <Header id={`title_${article.id}`} as={Link} to={{ pathname: '/full-article', state: { id: `${article.id}` } }}>{article.title}</Header>
+              <p id={`country_city_${article.id}`}><Icon name='map marker alternate' />{`${article.city}, ${article.country}`}</p>
+            </Segment>
+          )
+        })
+      )
+    }
 
-    )
 
     return (
       <Container>
@@ -64,17 +84,15 @@ class UserPage extends Component {
         <Grid columns='equal'>
           <Grid.Column>
             <p>Your published articles</p>
-
-
-
-
-
+            {publishedArticlesToDisplay}
           </Grid.Column>
           <Grid.Column>
             <p>Your unpublished articles</p>
+            {unpublishedArticlesToDisplay}
           </Grid.Column>
           <Grid.Column>
             <p>Your declined articles</p>
+            {declinedArticlesToDisplay}
           </Grid.Column>
         </Grid>
       </Container>
